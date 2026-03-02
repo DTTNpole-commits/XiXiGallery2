d8ef7339d39378801699cddb83f2110fcafb2188
DTTNpole-commits <snakerockyhu@gmail.com>
2026-03-02 08:19:09 +0000

    修正02

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/XiXiGallery2/'  // 必须大小写完全一致
+  base: '/XiXiGallery2/',   // ← 必须改成你的仓库名
 })
